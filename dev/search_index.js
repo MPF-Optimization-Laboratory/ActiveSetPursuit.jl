08dc90d0044d31bc5c06b034a15ea8624a134947
Documenter.jl <documenter@juliadocs.github.io>
2024-06-10 21:39:29 +0000

    build based on 518f60d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ActiveSetPursuit","category":"page"},{"location":"#ActiveSetPursuit","page":"Home","title":"ActiveSetPursuit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ActiveSetPursuit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ActiveSetPursuit]","category":"page"}]
+}
